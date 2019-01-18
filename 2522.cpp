c0ddb46f441b1e659ee2880fcbbd9c46a04805a7
Seong Hwa Hong <hshwaa@gmail.com>
2019-01-18 19:32:19 +0900

    별 찍기 - 12

---
+++ 2522.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int n;
+	int cnt = 0;
+	scanf("%d", &n);
+
+	for (int i = 1; i <= n; i++) {
+		for (int j = 1; j <= n-i; j++) {
+			printf(" ");
+		}
+		for (int j = 1; j <= i; j++) {
+			printf("*");
+		}
+		printf("\n");
+	}
+
+	for (int i = 1; i < n; i++) {
+		for (int j = 1; j <= i; j++) {
+			printf(" ");
+		}
+		for (int j = 1; j <= n - i; j++) {
+			printf("*");
+		}
+		printf("\n");
+	}
+}
