81f2db2acc8298e7015bf2685de084466f6e3f69
Seong Hwa Hong <hshwaa@gmail.com>
2019-01-26 01:16:50 +0900

    단어 길이 재기

---
+++ 2743.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <stack>
+#include <cstring>
+using namespace std;
+
+int main() {
+	char word[101];
+	scanf("%s", word);
+	int len = strlen(word);
+	printf("%d\n", len);
+}
